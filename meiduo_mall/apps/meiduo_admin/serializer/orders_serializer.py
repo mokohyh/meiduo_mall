04e1f8b9c65b8cb0a999f3e411cb9482452beb02
moko <dlxy_moko@163.com>
2019-10-25 21:47:37 +0800

    订单获取和修改状态

--- meiduo_mall/apps/meiduo_admin/serializer/orders_serializer.py
+++ meiduo_mall/apps/meiduo_admin/serializer/orders_serializer.py
@@ -1,10 +1,31 @@
 from rest_framework import serializers
 
-from orders.models import OrderInfo
+from goods.models import SKU
+from orders.models import OrderInfo,OrderGoods
+
+class SKUSerializer(serializers.ModelSerializer):
+    '''sku序列化器'''
+    class Meta:
+        model = SKU
+        fields = ('name','default_image')
+
+
+
+class OrderGoodsSerializer(serializers.ModelSerializer):
+    '''订单商品序列化器'''
+    sku = SKUSerializer()
+    class Meta:
+        model = OrderGoods
+        fields = ('count','price','sku')
+
 
 
 class OrderInfoSerializer(serializers.ModelSerializer):
+    '''订单信息序列化器'''
 
+    # 关联嵌套返回 用户表数据和订单商品表数据
+    user = serializers.StringRelatedField(read_only=True)
+    skus = OrderGoodsSerializer(many=True)
     class Meta:
         model = OrderInfo
         fields = "__all__"
--- meiduo_mall/apps/meiduo_admin/view/orders_view.py
+++ meiduo_mall/apps/meiduo_admin/view/orders_view.py
@@ -1,3 +1,5 @@
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from meiduo_admin.serializer.orders_serializer import OrderInfoSerializer
@@ -11,3 +13,21 @@ class OrderInfoView(ReadOnlyModelViewSet):
     queryset = OrderInfo.objects.all()
     serializer_class = OrderInfoSerializer
     pagination_class = UserPageNum
+
+
+    # 修改订单的方法
+    @action(methods=['put'], detail=True)
+    def status(self,request,pk):
+        # 获取订单对象
+        order = self.get_object()
+        # 获取status
+        status = request.data.get('status')
+        # 修改订单状态
+        order.status = status
+        order.save()
+        # 返回结果
+        ser = self.get_serializer(order)
+        return Response({
+            'order_id': order.order_id,
+            'status': status
+        })
